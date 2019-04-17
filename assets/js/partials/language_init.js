5e908fad7e488cd3fe7b82191d1caafa5dcc7320
Leon Kaucher <kaucher@teco.edu>
2019-04-17 12:54:59 +0200

    redirect only if page exists (XHR)

--- assets/js/partials/language_init.js
+++ assets/js/partials/language_init.js
@@ -1,11 +1,25 @@
 window.userLanguage = window.localStorage.userLanguage || navigator.language || navigator.userLanguage || "en";
 window.userLanguage = window.userLanguage.slice(0,2);
 window.localStorage.userLanguage = window.userLanguage;
+console.log(window.userLanguage);
+
 window.setUserLanguage = function(langCode){
     window.userLanguage = langCode;
     window.localStorage.userLanguage = window.userLanguage;
     window.redirectToLanguage();
 }
+
+window.runIfSiteExists = function (newPath, callback){
+    var http = new XMLHttpRequest();
+    http.open('HEAD', newPath);
+    http.onreadystatechange = function() {
+        if (this.readyState == this.DONE && this.status == 200) {
+            callback();
+        }
+    };
+    http.send();
+}
+
 window.redirectToLanguage = function(){
     var langPath = window.location.pathname.match(/\/(?<lang>\w{2})(?<langAgnosticPath>\/.*)/);
     var newPath = "";
@@ -19,16 +33,11 @@ window.redirectToLanguage = function(){
     else{
         newPath = "/" + window.userLanguage;
     }
-    if(newPath != window.location.pathname){
-        window.location.href = newPath;
+    if(newPath != window.location.pathname && newPath + "/" != window.location.pathname){
+        window.runIfSiteExists(newPath, function(){
+            window.location.replace(newPath);
+        })
     }
 }
 
-try{
-    var inPageNavigation = (new URL(document.referrer)).origin == window.location.origin
-    if(!inPageNavigation){
-        window.redirectToLanguage(window.userLanguage);
-    }
-} catch(e) {
-    window.redirectToLanguage(window.userLanguage);
-}
+window.redirectToLanguage(window.userLanguage);
